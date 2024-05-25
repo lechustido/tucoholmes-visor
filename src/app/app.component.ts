7d0a739d652af0a48b5f197fc20cb8bbe386c4ad
Gonzalo <gonzalo.carmenado@iesa.es>
2024-05-25 20:59:31 +0200

    Primeros pasos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,35 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatInputModule, MatFormFieldModule, FormsModule,CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+  public rawData!: any;
+  public savedData!: any;
+  public videoUrl!: SafeResourceUrl;
+  public showVideo: boolean;
   title = 'tucoholmes-visor';
+
+  constructor(
+    private sanitizer: DomSanitizer
+  ) {
+    this.showVideo = false;
+    console.log(this.savedData);
+  }
+
+  public onChangeValue(): void {
+    this.savedData = JSON.parse(this.rawData);
+    this.videoUrl =  this.sanitizer.bypassSecurityTrustResourceUrl(this.savedData.video);
+    this.showVideo = true;
+    console.log(this.savedData);
+  }
 }
