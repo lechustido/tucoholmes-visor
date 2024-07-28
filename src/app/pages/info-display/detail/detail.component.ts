ce7fc18692f9b4a592fbc89c1386b55eb0757b1f
Gonzalo <gonzalo.carmenado@iesa.es>
2024-07-28 19:32:43 +0200

    Primer filtro y control de video

--- src/app/pages/info-display/detail/detail.component.ts
+++ src/app/pages/info-display/detail/detail.component.ts
@@ -8,12 +8,21 @@ import { FormsModule } from '@angular/forms';
 import { MatInputModule } from '@angular/material/input';
 import { MatCardModule } from '@angular/material/card';
 import { SavedIssueData } from './model/saved-issue-data.model';
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
 
 @Component({
   selector: 'app-detail',
   templateUrl: './detail.component.html',
   standalone: true,
-  imports: [MatFormFieldModule, MatFormFieldModule, FormsModule,MatInputModule, CommonModule, MatCardModule],
+  imports: [
+    MatFormFieldModule,
+    MatFormFieldModule,
+    FormsModule,
+    MatInputModule,
+    CommonModule,
+    MatCardModule,
+    MatSlideToggleModule
+  ],
   styleUrl: './detail.component.scss',
 })
 export class DetailComponent {
@@ -21,15 +30,33 @@ export class DetailComponent {
   public rawData!: any;
   public savedData!: SavedIssueData;
   public videoUrl!: SafeResourceUrl;
+  /**
+   * Indica si el video se ha descompilado correctamente y se muestra el video (permite mostrar tambien el resto de elementos de la ventana)
+   * @type {boolean}
+   * @memberof DetailComponent
+   */
   public showVideo: boolean;
-  public title = 'tucoholmes-visor';
-  public markers:any = [
-  ]; // Puedes cambiar esto para usar tus datos dinámicos
- 
+  public markers: any = [];
+  /**
+   * Permite saber en que segundo se encuentra el video para aplicar el filtro "Sincronizado"
+   * @type {number}
+   * @memberof DetailComponent
+   */
+  public currentTimeVideo: number;
+  /**
+   * Indica si todos los datos mostrados se muestran de golpe o si van apareciendo según el video avance
+   *
+   * @type {boolean}
+   * @memberof DetailComponent
+   */
+  public sincFilterActive: boolean;
+
   videoDuration = 0;
 
   constructor(private sanitizer: DomSanitizer, public dialog: MatDialog) {
     this.showVideo = false;
+    this.currentTimeVideo = 0;
+    this.sincFilterActive = false;
   }
 
   public onChangeValue(): void {
@@ -37,17 +64,9 @@ export class DetailComponent {
     this.videoUrl = this.sanitizer.bypassSecurityTrustResourceUrl(
       this.savedData.video.base64
     );
-    console.log(this.savedData)
     this.videoDuration = this.savedData.video.timer;
     this.showVideo = true;
-    for (let i = 0; i < this.savedData.consoleLogs.length; i++) {
-      let console = this.savedData.consoleLogs[i];
-      this.markers.push({
-        timer: console.timer,
-        label: 'a'
-      })
-      
-    }
+    this.generateMarkersData();
   }
 
   public openDialog(restData: any): void {
@@ -59,13 +78,48 @@ export class DetailComponent {
       },
     });
 
-    dialogRef.afterClosed().subscribe((result) => {
+    dialogRef.afterClosed().subscribe((result: any) => {
       console.log(`Dialog result: ${result}`);
     });
   }
 
-
   public calculatePosition(timer: number): number {
     return (timer / this.videoDuration) * 100;
   }
+
+  //#region Funciones comunes
+  public generateMarkersData(): void {
+    for (let i = 0; i < this.savedData.consoleLogs.length; i++) {
+      let console = this.savedData.consoleLogs[i];
+      this.markers.push({
+        timer: console.timer,
+        label: 'c',
+      });
+    }
+    for (let i = 0; i < this.savedData.consoleError.length; i++) {
+      let consoleError = this.savedData.consoleError[i];
+      this.markers.push({
+        timer: consoleError.timer,
+        label: 'e',
+      });
+    }
+    for (let i = 0; i < this.savedData.requests.length; i++) {
+      let requests = this.savedData.requests[i];
+      this.markers.push({
+        timer: requests.timer,
+        label: 'h',
+      });
+    }
+  }
+
+  seekTo(timer: number): void {
+    this.videoElementCR.nativeElement.currentTime = timer;
+    this.videoElementCR.nativeElement.play();
+  }
+
+  public onTimeUpdate(): void {
+    this.currentTimeVideo = this.videoElementCR.nativeElement.currentTime;
+    // Aquí puedes realizar cualquier acción adicional con el tiempo actual
+  }
+  //#endregion Funciones comunes
 }
